9a8415505a35c66d8cc32447c62fe23c3b244e24
devinacker <d@revenant1.net>
2015-03-31 01:14:12 -0400

    super fx properties

--- bsnes/snes/chip/superfx/debugger/debugger.cpp
+++ bsnes/snes/chip/superfx/debugger/debugger.cpp
@@ -102,8 +102,74 @@ bool SFXDebugger::property(unsigned id, string &name, string &value) {
     return true; \
   }
 
-  // TODO: all of these
+  item("$3000-$301f", "")
+  item("Register R0", string("0x", hex<4>(regs.r[0])));
+  item("Register R1", string("0x", hex<4>(regs.r[1])));
+  item("Register R2", string("0x", hex<4>(regs.r[2])));
+  item("Register R3", string("0x", hex<4>(regs.r[3])));
+  item("Register R4", string("0x", hex<4>(regs.r[4])));
+  item("Register R5", string("0x", hex<4>(regs.r[5])));
+  item("Register R6", string("0x", hex<4>(regs.r[6])));
+  item("Register R7", string("0x", hex<4>(regs.r[7])));
+  item("Register R8", string("0x", hex<4>(regs.r[8])));
+  item("Register R9", string("0x", hex<4>(regs.r[9])));
+  item("Register R10", string("0x", hex<4>(regs.r[10])));
+  item("Register R11", string("0x", hex<4>(regs.r[11])));
+  item("Register R12", string("0x", hex<4>(regs.r[12])));
+  item("Register R13", string("0x", hex<4>(regs.r[13])));
+  item("Register R14", string("0x", hex<4>(regs.r[14])));
+  item("Register R15", string("0x", hex<4>(regs.r[15])));
   
+  item("$3030", "")
+  item("Status Flag Register (SFR)", string("0x", hex<4>(regs.sfr)))
+  
+  item("$3033", "")
+  item("Backup RAM Register (BRAMR)", string("0x", hex<2>(regs.bramr)))
+
+  item("$3034", "")
+  item("Program Bank Register", string("0x", hex<2>(regs.pbr)))
+  
+  item("$3037", "")
+  item("Multiplier Speed", (regs.cfgr & 0x20) ? "High Speed" : "Normal")
+  item("IRQ Enable", (regs.cfgr & 0x80) ? "Disabled" : "Enabled")
+  
+  item("$3038", "")
+  item("Screen Base Register (SCBR)", string("0x", hex<2>(regs.scbr)))
+  
+  item("$3039", "")
+  item("Clock Register (CLSR)", regs.clsr ? "21.4 MHz" : "10.7 MHz")
+  
+  item("$303a", "")
+  string md, ht;
+  
+  switch (regs.scmr & 0x3) {
+  case 0: md = "4 colors"; break;
+  case 1: md = "16 colors"; break;
+  case 2: md = "Invalid"; break;
+  case 3: md = "256 colors"; break;
+  }
+  
+  switch (regs.scmr & 0x24) {
+  case 0x00: ht = "128 pixels"; break;
+  case 0x04: ht = "160 pixels"; break;
+  case 0x20: ht = "192 pixels"; break;
+  case 0x24: ht = "OBJ mode"; break;
+  }
+  
+  item("Color Mode", md)
+  item("Screen Height", ht);
+  item("Game Pak WRAM Access", regs.scmr & 0x8 ? "Super FX" : "SNES")
+  item("Game Pak ROM Access", regs.scmr & 0x10 ? "Super FX" : "SNES")
+  
+  item("$303b", "")
+  item("Version Register (VCR)", string("0x", hex<2>(regs.vcr)))
+  
+  item("$303c", "")
+  item("Game Pak RAM Bank", regs.rambr ? "0x71" : "0x70")
+  
+  item("$303e-$303f", "")
+  item("Cache Base Register (CBR)", string("0x", hex<4>(regs.cbr)))
+
   #undef item
   return false;
 }
