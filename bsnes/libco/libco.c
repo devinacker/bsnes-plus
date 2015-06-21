a2197beeac8e751dfb6ec22b8aa0e12a03e86ba5
Alex W. Jackson <alex.w.jackson@gmail.com>
2015-06-21 10:56:02 -0400

    Update libco

--- bsnes/libco/libco.c
+++ bsnes/libco/libco.c
@@ -1,6 +1,5 @@
 /*
   libco
-  auto-selection module
   license: public domain
 */
 
@@ -9,28 +8,36 @@
   #define LIBCO_NO_INLINE_ASM
 #endif
 
-#if defined(__GNUC__) && defined(__i386__)
-  #if defined(LIBCO_NO_INLINE_ASM)
-    #include "x86.c"
+#if defined(__clang__) || defined(__GNUC__)
+  #if defined(__i386__)
+    #if defined(LIBCO_NO_INLINE_ASM)
+      #include "x86.c"
+    #else
+      #include "x86_gcc.c"
+    #endif
+  #elif defined(__amd64__)
+    #if defined(LIBCO_NO_INLINE_ASM)
+      #include "amd64.c"
+    #else
+      #include "amd64_gcc.c"
+    #endif
+  #elif defined(__arm__)
+    #include "arm.c"
+  #elif defined(_ARCH_PPC)
+    #include "ppc.c"
+  #elif defined(_WIN32)
+    #include "fiber.c"
   #else
-    #include "x86_gcc.c"
+    #include "sjlj.c"
   #endif
-#elif defined(__GNUC__) && defined(__amd64__)
-  #if defined(LIBCO_NO_INLINE_ASM)
+#elif defined(_MSC_VER)
+  #if defined(_M_IX86)
+    #include "x86.c"
+  #elif defined(_M_AMD64)
     #include "amd64.c"
   #else
-    #include "amd64_gcc.c"
+    #include "fiber.c"
   #endif
-#elif defined(__GNUC__) && defined(_ARCH_PPC)
-  #include "ppc.c"
-#elif defined(__GNUC__)
-  #include "sjlj.c"
-#elif defined(_MSC_VER) && defined(_M_IX86)
-  #include "x86.c"
-#elif defined(_MSC_VER) && defined(_M_AMD64)
-  #include "amd64.c"
-#elif defined(_MSC_VER)
-  #include "fiber.c"
 #else
   #error "libco: unsupported processor, compiler or operating system"
 #endif
